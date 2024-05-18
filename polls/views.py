2b5bcb414fc3e3feae9643047760b4181a3cd04e
HoangThien77 <pewpew1232002@gmail.com>
2024-05-18 10:33:44 +0700

    add option

--- polls/views.py
+++ polls/views.py
@@ -34,6 +34,8 @@ def vote(request, question_id):
             question.option_three_count += 1
         elif selected_choice == "option4":
             question.option_three_count += 1
+        elif selected_choice == "option5":
+            question.option_three_count += 1
         else:
             return HttpResponse("Invalid choice")
         question.save()
